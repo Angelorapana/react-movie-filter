cd01aec1261869e504bdc6611465df5401127294
angelorapana <angelorapana@icloud.com>
2026-03-06 14:30:40 +0100

    Utilizzo di useEffect per la reinderizzazione al cambio stato della variabile selectGenere

--- Movie-Filter/src/App.jsx
+++ Movie-Filter/src/App.jsx
@@ -1,4 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+
+
 
 const film = [{ title: 'Inception', genre: 'Fantascienza' },
 { title: 'Il Padrino', genre: 'Thriller' },
@@ -12,6 +15,15 @@ const film = [{ title: 'Inception', genre: 'Fantascienza' },
 function App() {
   const [selectGenere, setSelectGenere] = useState("");
   const [filmVisibile, setFilmVisibile] = useState(film);
+
+  useEffect(() => {
+    if (selectGenere === "") { setFilmVisibile(film) }
+    else {
+      const filtered = film.filter((elemento) => elemento.genre === selectGenere)
+      setFilmVisibile(filtered)
+    }
+    [selectGenere]
+  })
 
   return (
     <h1>ciaooo</h1>
