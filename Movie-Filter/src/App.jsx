a13508ebac54d1176c3d573e65443320e74c2887
angelorapana <angelorapana@icloud.com>
2026-03-06 12:43:22 +0100

    Dichiarazione delle variabili con stato

--- Movie-Filter/src/App.jsx
+++ Movie-Filter/src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react'
 
+const film = [{ title: 'Inception', genre: 'Fantascienza' },
+{ title: 'Il Padrino', genre: 'Thriller' },
+{ title: 'Titanic', genre: 'Romantico' },
+{ title: 'Batman', genre: 'Azione' },
+{ title: 'Interstellar', genre: 'Fantascienza' },
+{ title: 'Pulp Fiction', genre: 'Thriller' },]
+
+
 
 function App() {
-
+  const [selectGenere, setSelectGenere] = useState("");
+  const [filmVisibile, setFilmVisibile] = useState(film);
 
   return (
     <h1>ciaooo</h1>
